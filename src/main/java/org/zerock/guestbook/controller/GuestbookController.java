743b85338aeee98e0467b21129340e44fe7b3961
sjiyeong12 <105151230+sjiyeong12@users.noreply.github.com>
2022-07-13 17:13:06 +0900

    commit

--- src/main/java/org/zerock/guestbook/controller/GuestbookController.java
+++ src/main/java/org/zerock/guestbook/controller/GuestbookController.java
@@ -3,6 +3,7 @@ package org.zerock.guestbook.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
@@ -39,10 +40,43 @@ public class GuestbookController {
 
     @PostMapping("/register")
     public String registerPost(GuestbookDTO dto, RedirectAttributes ra) {
+        // RedirectAttributes는 다음 페이지로 값을 넘겨줄 수 있다. model처럼..!
         log.info("register post...");
         Long gno = gbService.register(dto);
         ra.addFlashAttribute("msg", gno);
+        // 플래시처럼 데이터를 한번만 전송해준다. 한번만 넘겨주고 다음에 넘겨줄 때는 못 받게 만들어 줌(일회성) ("메세지", 글번호)
         return "redirect:/guestbook/list";
+        // redirect:를 붙인 이유 -> 32행의 값을 넘겨줘야함. 하지만 코드가 중복되는 것을 방지해야함.
+        // 그리고 "/guestbook/list"이렇게만 적으면 리소스라서 값을 못 받음. 그래서 리소스가 아니라 주소로 보내야 하기 때문에
+        // redirect:를 붙임
+    }
+
+    @GetMapping({ "/read", "/modify" })
+    public void read(long gno, Model model,
+            @ModelAttribute("requestDTO") PageRequestDTO requestDTO) {
+        // 커맨드 객체로 받은 것은 다음 페이지에도 넘어감.
+        log.info("read..........gno:" + gno);
+        GuestbookDTO dto = gbService.read(gno);
+        model.addAttribute("dto", dto);
+    }
+
+    @PostMapping("/remove")
+    public String remove(long gno, RedirectAttributes ra) {
+        log.info("remove....." + gno);
+        gbService.read(gno);
+        ra.addFlashAttribute("msg", gno);
+        return "redirect:/guestbook/list";
+    }
+
+    @PostMapping("/modify")
+    public String modifyPost(GuestbookDTO dto, RedirectAttributes ra,
+            PageRequestDTO requestDTO) {
+        log.info("modify...." + dto);
+        gbService.modify(dto);
+        ra.addFlashAttribute("msg", dto.getGno() + "");
+        ra.addAttribute("gno", dto.getGno());
+        ra.addAttribute("page", requestDTO.getPage());
+        return "redirect:/guestbook/read";
     }
 
 }
--- src/main/java/org/zerock/guestbook/service/GuestbookServiceImpl.java
+++ src/main/java/org/zerock/guestbook/service/GuestbookServiceImpl.java
@@ -1,5 +1,6 @@
 package org.zerock.guestbook.service;
 
+import java.util.Optional;
 import java.util.function.Function;
 
 import org.springframework.data.domain.Page;
@@ -22,6 +23,12 @@ public class GuestbookServiceImpl implements GuestbookService {
     private final GuestbookRepository gbRepository;
 
     @Override
+    public GuestbookDTO read(Long gno) {
+        Optional<Guestbook> result = gbRepository.findById(gno);
+        return result.isPresent() ? entityToDTO(result.get()) : null;
+    }
+
+    @Override
     public Long register(GuestbookDTO dto) {
         log.info("register dto:" + dto);
         Guestbook entity = dtoToEntity(dto);
@@ -40,5 +47,26 @@ public class GuestbookServiceImpl implements GuestbookService {
             }
         };
         return new PageResultDTO<>(result, fn);
+    }
+
+    @Override
+    public void remove(Long gno) {
+        log.info("remove..." + gno);
+        gbRepository.deleteById(gno);
+
+    }
+
+    @Override
+    public void modify(GuestbookDTO dto) {
+        log.info("modify..." + dto);
+        // Entity를 먼저 찾는 이유: Entity가 있어야 부분 변경이 가능
+        Optional<Guestbook> result = gbRepository.findById(dto.getGno());
+        if (result.isPresent()) {
+            Guestbook entity = result.get();
+            entity.changeTitle(dto.getTitle()); // 부분만 바꿈
+            entity.changeCOntent(dto.getContent()); // 부분만 바꿈
+            gbRepository.save(entity);
+        }
+
     }
 }
